1f56a281c83ffadd139a9edee878e480a4cf88c3
Maarten-vd-Sande <maartenvandersande@hotmail.com>
2020-12-10 17:19:55 +0100

    maybe like so?

--- seq2science/scripts/get_genome_annotation.py
+++ seq2science/scripts/get_genome_annotation.py
@@ -5,33 +5,37 @@ import os
 import contextlib
 
 import genomepy
-from snakemake.logging import logger
 
 
 with open(snakemake.log[0], "w") as log:
     with contextlib.redirect_stdout(log), contextlib.redirect_stderr(log):
+        try:
+            provider = snakemake.params.providers[snakemake.wildcards.raw_assembly]["annotation"]
 
-        provider = snakemake.params.providers[snakemake.wildcards.raw_assembly]["annotation"]
-        if provider not in ["ensembl", "ucsc", "ncbi"]:
-            logger.error("Seq2science database error. Please run 'seq2science --clean' and try again")
-            raise TerminatedException
-            
-        p = genomepy.ProviderBase.create(provider)
+            p = genomepy.ProviderBase.create(provider)
 
-        kwargs = dict()
-        if provider == "ucsc" and p.get_annotation_download_link(
-                name=snakemake.wildcards.raw_assembly, kwargs={"ucsc_annotation_type": "ensembl"}):
-            kwargs = {"ucsc_annotation_type": "ensembl"}
+            kwargs = dict()
+            if provider == "ucsc" and p.get_annotation_download_link(
+                    name=snakemake.wildcards.raw_assembly, kwargs={"ucsc_annotation_type": "ensembl"}):
+                kwargs = {"ucsc_annotation_type": "ensembl"}
 
-        p.download_annotation(
-            name=snakemake.wildcards.raw_assembly,
-            genomes_dir=snakemake.params.genome_dir,
-            kwargs=kwargs)
+            p.download_annotation(
+                name=snakemake.wildcards.raw_assembly,
+                genomes_dir=snakemake.params.genome_dir,
+                kwargs=kwargs)
 
-        # sanitize the annotations
-        genome = genomepy.Genome(snakemake.wildcards.raw_assembly, snakemake.params.genome_dir)
-        genomepy.utils.sanitize_annotation(genome)
+            # sanitize the annotations
+            genome = genomepy.Genome(snakemake.wildcards.raw_assembly, snakemake.params.genome_dir)
+            genomepy.utils.sanitize_annotation(genome)
 
-        # TODO remove after fixing inconsistency in gp
-        if os.path.exists(snakemake.output.gtf[0][:-3]):
-            genomepy.utils.gzip_and_name(snakemake.output.gtf[0][:-3])
+            # TODO remove after fixing inconsistency in gp
+            if os.path.exists(snakemake.output.gtf[0][:-3]):
+                genomepy.utils.gzip_and_name(snakemake.output.gtf[0][:-3])
+        except Exception as e:
+            print(e)
+            print("\nSomething went wrong while downloading the genome (see error message above). "
+                  "When this happens it is almost always because we had troubles connecting to the"
+                  "servers hosting the genome assemblies. Usually this is resolved by just running seq2science"
+                  "again, either immediately or in a couple hours.\n\n"
+                  "If the problem persists you could try running `seq2science clean` and see if that resolves the "
+                  "issue.")
