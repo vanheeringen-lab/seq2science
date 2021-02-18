119e01923d6138d1688212d3d6b666d7484b2f02
Maarten-vd-Sande <maartenvandersande@hotmail.com>
2021-02-18 10:30:24 +0100

    like this?

--- seq2science/cli.py
+++ seq2science/cli.py
@@ -21,6 +21,7 @@ def _import():
     """
     this function serves that we can do imports as late as possible, for faster auto-completion
     """
+    global webbrowser, re, contextlib, yaml, psutil, snakemake, logger, setup_logger, xdg
     import webbrowser
     import re
     import contextlib
