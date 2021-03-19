d6941fceaee89864f2d9d82b8e86949cb97dd0df
siebrenf <siebrenf@gmail.com>
2021-03-19 13:28:04 +0100

    added --yes flag to s2s init

--- seq2science/cli.py
+++ seq2science/cli.py
@@ -121,6 +121,13 @@ def seq2science_parser(workflows_dir="./seq2science/workflows/"):
         help="The path to the directory where to initialise the config and samples files.",
     )
 
+    init.add_argument(
+        "-y", "--yes",
+        default=False,
+        help="Overwrite existing samples.tsv and config.yaml silently.",
+        action="store_true",
+    )
+
     global core_arg
     core_arg = run.add_argument(
         "-j", "--cores",
@@ -205,7 +212,7 @@ def _init(args, workflows_dir, config_path):
         dest = os.path.join(os.path.dirname(config_path), file)
 
         copy_file = True
-        if os.path.exists(dest):
+        if os.path.exists(dest) and args.yes is False:
             choices = {"yes": True, "y": True, "no": False, "n": False}
 
             sys.stdout.write(
