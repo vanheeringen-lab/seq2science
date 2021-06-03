cadfc23d759dbadfe0f22b5e86685be2cdb874e7
Siebren Frölich <48289046+siebrenf@users.noreply.github.com>
2021-06-03 12:56:54 +0200

    refactored deseq2 scripts (#706)
    * refactored deseq2 scripts
    
    * style changes
    
    * docstring fix
    
    * added cluster plots to get_qc_files
    
    * fix get_qc_files
    
    * fixed old issue where deseq2 made each contrast for each assembly
    
    * fix misnamed variable
    
    * fix snakefiles atac&chip
    
    * why did that fail?
    
    * change multiQC order
    
    * keep tidydata.fa.fai
    
    * added dumb test-prints
    
    * R cant handle trailing commas
    
    * fixed tests & expand_contrasts
    
    * missing test
    
    * removed old cluster code
    
    * missed conversion to replicate(S)
    
    * clearly name deepTools and DESeq2 heatmaps
    
    * fix qc reference
    
    * include backup columns
    
    * show reason on failed tests
    
    * show reason on failed tests
    
    * fix timestamp desync
    
    * fix qc reference - part 2
    
    * fix qc reference - part 3
    
    * broken references
    
    * temp shorten tests
    
    * temp shorten tests
    
    * dumb print statements again
    
    * row.names perhaps?
    
    * assign outside ifelse perhaps?
    
    * if(){}else{} perhaps?
    
    * removed more trailing commas
    
    * test cor()
    
    * fix cor & refactored R library imports
    
    * restored jenkinsfile
    
    * replaced most ifelse /w if(){}else{} (ifelse truncates return values)
    
    * fixed if(){}else{} changes in PCA
    
    * use descriptive names if available
    
    * mildly clearer
    
    Co-authored-by: Maarten-vd-Sande <maartenvandersande@hotmail.com>

---
+++ seq2science/scripts/deseq2/clustering.R
@@ -0,0 +1,100 @@
+suppressMessages({
+  library(DESeq2)
+})
+
+# snakemake variables
+threads        <- snakemake@threads[[1]]
+log_file       <- snakemake@log[[1]]
+counts_file    <- snakemake@input[[1]]
+samples_file   <- snakemake@params$samples
+replicates     <- snakemake@params$replicates
+scripts_dir    <- snakemake@params$scripts_dir
+assembly       <- snakemake@wildcards$assembly
+out_png        <- snakemake@output[[1]]
+
+# log all console output
+log <- file(log_file, open="wt")
+sink(log)
+sink(log, type="message")
+
+# load custom functions
+source(file.path(scripts_dir, "utils.R"))
+
+# log all variables for debugging purposes
+cat('# variables used for this analysis:\n')
+cat('threads      <- ',  threads,      '\n')
+cat('counts_file  <- "', counts_file,  '"\n', sep = "")
+cat('samples_file <- "', samples_file, '"\n', sep = "")
+cat('replicates   <- ',  replicates,   '\n')
+cat('assembly     <- "', assembly,     '"\n', sep = "")
+cat('out_png      <- "', out_png,      '"\n', sep = "")
+cat('\n')
+
+cat('Sessioninfo:\n')
+sessionInfo()
+cat('\n')
+
+
+## obtain coldata, the metadata input for DESeq2
+samples <- parse_samples(samples_file, assembly, replicates)
+
+# filter for assembly, remove NAs and add random variables (not needed for blind clustering)
+cols <- if("descriptive_name" %in% colnames(samples)){c('assembly', 'descriptive_name')}else{'assembly'}
+coldata  <- samples[cols]
+coldata['assembly'] <- factor(as.character(seq_len(nrow(coldata))))
+
+# filter counts to speed up DESeq
+counts <- read.table(counts_file, row.names = 1, header = T, stringsAsFactors = F, sep = '\t', check.names = F)
+reduced_counts <- counts[rowSums(counts) > 0, colnames(counts) %in% rownames(coldata)]
+
+
+## DESeq2
+dds <- run_deseq2(reduced_counts, coldata, ~ 1, threads)
+
+
+## Heatmaps
+# list of aesthetics for all heatmaps
+heatmap_aes <- heatmap_aesthetics(nrow(coldata))
+
+# transform counts for all heatmaps
+vsd <- varianceStabilizingTransformation(dds, blind = TRUE)
+vsdMatrix <- assay(vsd)
+
+# Heatmap of the sample-to-sample distances
+# see http://www.bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#heatmap-of-the-sample-to-sample-distances
+mat <- as.matrix(dist(t(vsdMatrix)))
+mat <- heatmap_names(mat, coldata)
+
+title <- 'Sample distance clustering (blind)'
+legend_aes <- list(
+  "breaks" = c(min(mat), mean(c(min(mat), max(mat))), max(mat)),
+  "labels" = c("low",    "distance\n(euclidean)",     "high")
+)
+out_pdf <- sub(".png", ".pdf", out_png)
+heatmap_plot(mat, title, heatmap_aes, legend_aes, out_pdf)
+
+# convert pdf to png (required for MULTIQC)
+pdf2png(out_pdf, out_png)
+
+
+# Heatmaps of various correlation matrices
+for (method in list("spearman", "pearson")){
+  name <- paste(method, 'correlation clustering')
+
+  # Kendall is also available, but is computationally much more complex
+  mat <- as.matrix(cor(vsdMatrix, method=method))
+  mat <- heatmap_names(mat, coldata)
+
+  # capitalize method name
+  title <- paste0(toupper(substring(name, 1, 1)), substring(name, 2))
+  legend_aes <- list(
+    "breaks" = NA,
+    "labels" = NA
+  )
+  out_pdf_cor <- sub("sample_distance_clustering", gsub(" ", "_", name), out_pdf)
+  heatmap_plot(mat, title, heatmap_aes, legend_aes, out_pdf_cor)
+
+  # convert pdf to png (required for MULTIQC)
+  out_png_cor <- sub(".pdf", ".png", out_pdf_cor)
+  pdf2png(out_pdf_cor, out_png_cor)
+}
