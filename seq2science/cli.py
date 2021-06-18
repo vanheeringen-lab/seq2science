5842e75b7869eae549775a93e8083c08cfa46c14
Siebren Frölich <48289046+siebrenf@users.noreply.github.com>
2021-06-18 16:04:28 +0200

    Update cli.py

--- seq2science/cli.py
+++ seq2science/cli.py
@@ -282,9 +282,8 @@ def _run(args, base_dir, workflows_dir, config_path):
                    "printreason": args.reason,
                    "keepgoing": args.keep_going,
                    "unlock": args.unlock,
-                   "force_incomplete": args.rerun_incomplete,
-                   # "scheduler": "greedy"
-                  }
+                   "force_incomplete": args.rerun_incomplete}
+                   # "scheduler": "greedy"}
 
     # get the additional snakemake options
     snakemake_options = args.snakemakeOptions if args.snakemakeOptions is not None else dict()
@@ -387,9 +386,8 @@ def _explain(args, base_dir, workflows_dir, config_path):
     parsed_args = {"snakefile": os.path.join(workflows_dir, args.workflow.replace("-", "_"), "Snakefile"),
                    "dryrun": True,
                    "forceall": True,
-                   "quiet": False,
+                   "quiet": False}
                    # "scheduler": "greedy"
-                  }
 
     # get the additional snakemake options
     snakemake_options = args.snakemakeOptions if args.snakemakeOptions is not None else dict()
