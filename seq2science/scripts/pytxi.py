1b662c0e6af7a9d97326cc584d5f20432b784318
Siebren Frölich <48289046+siebrenf@users.noreply.github.com>
2022-04-21 20:17:48 +0200

    RNA-seq always produces TPM normalized counts (#835)
    * RNA-seq always produces TPM normalized counts
    
    * robustness
    
    * fixes
    
    * pin pytxi + fix readme.txt issue
    
    * fixes
    
    * better error message

--- seq2science/scripts/pytxi.py
+++ seq2science/scripts/pytxi.py
@@ -4,7 +4,7 @@ convert TPM to gene counts
 import os
 import contextlib
 
-from genomepy import Annotation
+from genomepy import Annotation, Genome, search
 import pandas as pd
 import pytxi
 
@@ -12,9 +12,12 @@ import pytxi
 logfile = snakemake.log[0]
 fnames = snakemake.input.cts
 assembly = snakemake.wildcards.assembly
-species = snakemake.input.fa[0]
+fa = snakemake.input.fa[0]
+gtf = snakemake.input.gtf[0]
 from_gtf = snakemake.params.from_gtf
-output = snakemake.output[0]
+out_counts = snakemake.output.counts[0]
+out_tpms = snakemake.output.tpms[0]
+out_lengths = snakemake.output.lengths[0]
 sample_names = snakemake.params.names
 
 # redirect all messages to a logfile
@@ -25,27 +28,61 @@ with open(logfile, "a") as log:  # appending because we mix streams (tqdm, logge
         pytxi.logger.add(logfile)
 
         fnames = [f"{d}/quant.sf" for d in fnames]
-        outdir = os.path.dirname(output)
+        outdir = os.path.dirname(out_counts)
         os.makedirs(outdir, exist_ok=True)
 
         tx2gene = None
         if from_gtf:
-            # check if we can convert transcript ids to symbols (gene names) with the GTF file
-            ann = Annotation(species)
-            dct = ann.gtf_dict("transcript_id", "gene_name")
-            df = pd.DataFrame.from_dict(dct, orient="index", columns=["gene_name"])
-            df.index.name = "transcript_id"
-            lendf = len(set(df.index))
-            lengenes = len(ann.genes("bed"))
-            pytxi.logger.info(
-                f"GTF has {lendf} transcript ids, TPMs have {lengenes} transcript ids"
-            )
-            if lendf >= 0.9*lengenes:
+            ann = Annotation(gtf)
+
+            # check if we can convert transcript ids to symbols with the GTF file
+            test = ann.gtf["attribute"].head(100)
+            attribute = None
+            if any(test.str.contains("gene_name")):
+                attribute = "gene_name"
+            elif any(test.str.contains("gene_id")):
+                attribute = "gene_id"
+
+            if attribute is not None:
+                dct = ann.gtf_dict("transcript_id", attribute)
+                df = pd.DataFrame.from_dict(dct, orient="index", columns=[attribute])
+                df.index.name = "transcript_id"
                 tx2gene = f"{outdir}/tx2gene.tsv"
                 df.to_csv(tx2gene, sep="\t")
 
+                # print some metrics
+                lendf = len(set(df.index))
+                lenbed = len(ann.genes("bed"))
+                pytxi.logger.info(
+                    f"GTF has {lendf} transcript ids, TPMs have {lenbed} transcript ids"
+                )
+            else:
+                print("GTF does not contain gene_name or gene_id! Using MyGene.info...")
+
+        taxonomy = None
+        if tx2gene is None:
+            # get the taxonomy from the README.txt
+            g = Genome(fa, build_index=False)
+            taxonomy = g.tax_id
+
+            # fallback: search the genomepy database for the taxonomy id
+            if not isinstance(taxonomy, int):
+                hits = list(search(assembly))
+                for hit in hits:
+                    if assembly == hit[0] and isinstance(hit[3], int):
+                        taxonomy = hit[3]
+                        break
+
+            if not isinstance(taxonomy, int):
+                raise ValueError(
+                    f"No taxonomy ID was found for {g.name}! "
+                    "To fix this manually, add the line 'tax_id: 1234' "
+                    f"(with the correct ID) to {g.readme_file}"
+                )
+
         txi = pytxi.TxImport()
-        txi.import_files(fnames, sample_names, tx2gene, species)
-        txi.abundance.to_csv(f"{outdir}/{assembly}-TPM.tsv", sep="\t")
-        txi.counts.to_csv(f"{outdir}/{assembly}-counts.tsv", sep="\t")
-        txi.length.to_csv(f"{outdir}/{assembly}-gene_lengths.tsv", sep="\t")
+        txi.import_files(fnames, sample_names, tx2gene, taxonomy)
+
+        txi.abundance.to_csv(out_tpms, sep="\t")
+        txi.counts.to_csv(out_counts, sep="\t")
+        txi.length.to_csv(out_lengths, sep="\t")
