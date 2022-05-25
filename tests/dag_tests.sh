0963bd7fdf6bac70b22a9eaae1fcaa382fb46e4a
Maarten-vd-Sande <maartenvandersande@hotmail.com>
2022-05-25 14:36:28 +0200

    more wip

--- tests/dag_tests.sh
+++ tests/dag_tests.sh
@@ -309,6 +309,10 @@ if [ $1 = "atac-seq" ]; then
   assert_rulecount $1 genrich_pileup 4
   assert_rulecount $1 macs2_callpeak 4
 
+  printf "\ngimme maelstrom\n"
+  seq2science run atac-seq -nr --configfile tests/$WF/macs2.yaml --snakemakeOptions quiet=True config={samples:tests/atac_seq/complex_samples2.tsv,create_qc_report:True,run_gimme_maelstrom:True} | tee tests/local_test_results/${1}_dag
+  assert_rulecount $1 gimme_maelstrom 1
+  assert_rulecount $1 motif2factors 1
   test_ran=1
 fi
 
