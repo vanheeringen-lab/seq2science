f56fa114cde5aeecb56593160a88e3c9cee40c6b
Maarten van der Sande <maartenvandersande@hotmail.com>
2022-11-28 12:38:48 +0100

    small bugfix

--- seq2science/scripts/combine_biological_reps.py
+++ seq2science/scripts/combine_biological_reps.py
@@ -19,11 +19,12 @@ with open(str(snakemake.log), "w") as f:
                                 True,
                                 False)
         breps = snakemake.params.breps
-
-        groups = [breps.index(samples[samples["descriptive_name"] == trep]["biological_replicates"].values) for trep in counts.columns]
-
-        newcounts = counts.groupby(groups, axis=1).mean()
-        newcounts.columns = breps
+        if "biological_replicates" in samples.columns:
+            groups = [breps.index(samples[samples["descriptive_name"] == trep]["biological_replicates"].values) for trep in counts.columns]
+            newcounts = counts.groupby(groups, axis=1).mean()
+            newcounts.columns = breps
+        else:
+            newcounts = counts.copy()
 
         with open(snakemake.output[0], "w") as f:
             f.write(newcounts.to_csv(header=True, sep="\t"))
